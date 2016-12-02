1e988a6a98f95009fe312a33963f58c9c239db4d
Iury Lamonie - NUDES <iury.lamonie@hotmail.com>
2016-12-02 16:41:09 -0300

    Criação do HelloApp.

---
+++ IFZAP/HelloApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HelloApp.Resources;
+
+namespace HelloApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
