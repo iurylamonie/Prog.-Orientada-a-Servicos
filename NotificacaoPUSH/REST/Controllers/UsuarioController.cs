cd73c6d109c8ff86156858fcd5a49231d0cf9195
Iury Lamonie - NUDES <iury.lamonie@hotmail.com>
2016-12-05 14:18:03 -0300

    Criação do REST do Exemplo.

---
+++ NotificacaoPUSH/REST/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace REST.Controllers
+{
+    [RoutePrefix("api/Usuario")]
+    public class UsuarioController : ApiController
+    {
+        [AcceptVerbs("POST")]
+        [Route("Inserir")]
+        public void Inserir([FromBody] string conteudo)
+        {
+            Models.CelularDataContext cdc = new Models.CelularDataContext();
+            Models.Usuario usuario = JsonConvert.DeserializeObject<Models.Usuario>(conteudo);
+            var r = from u in cdc.Usuarios
+                    where string.Equals(u.Nome, usuario.Nome)
+                    select u;
+            if (r == null)
+            {
+                cdc.Usuarios.InsertOnSubmit(usuario);
+            }
+            else
+            {
+                var x = r.FirstOrDefault();
+            }
+            cdc.SubmitChanges();
+        }
+    }
+}
