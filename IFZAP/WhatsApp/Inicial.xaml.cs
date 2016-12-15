3b20b56578f34f59b278bb8fd0bb52e99ca0278d
Iury Lamonie <iury.lamonie@hotmail.com>
2016-12-15 18:02:19 -0300

    WhasApp
    Criação da tela oficial do projeto.

---
+++ IFZAP/WhatsApp/Inicial.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace WhatsApp
+{
+    public partial class Inicial : PhoneApplicationPage
+    {
+        public Inicial()
+        {
+            InitializeComponent();
+            List<Models.TUsuario> listUser = new List<Models.TUsuario>();
+            listUser.Add(new Models.TUsuario { Nome = "Iury", Descrição = "Aluno" });
+            listUser.Add(new Models.TUsuario { Nome = "Gilbert", Descrição = "Professor" });
+            listUser.Add(new Models.TUsuario { Nome = "George", Descrição = "Professor" });
+            listUsuarios.ItemsSource = listUser;
+            listUsuarios.SelectedItem = 1;
+        }
+    }
+}
---
+++ IFZAP/WhatsApp/Models/TUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsApp.Models
+{
+    class TUsuario
+    {
+        public string Nome { get; set; }
+        public string Descrição { get; set; }
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", Nome, Descrição);
+        }
+    }
+}
